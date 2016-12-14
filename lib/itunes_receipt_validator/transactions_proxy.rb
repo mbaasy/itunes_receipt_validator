f0df2a01a4ca180351a781e2ab6a9603302c87a1
Marc Greenstock <marc@marcgreenstock.com>
2016-12-14 12:45:36 +0100

    use sort_by

--- lib/itunes_receipt_validator/transactions_proxy.rb
+++ lib/itunes_receipt_validator/transactions_proxy.rb
@@ -5,8 +5,7 @@ module ItunesReceiptValidator
   # ItunesReceiptValidator::TransactionsProxy
   class TransactionsProxy < Array
     def self.import(array, receipt)
-      new array.map { |t| Transaction.new(t, receipt) }
-               .sort { |a, b| a.purchased_at <=> b.purchased_at }
+      new array.map { |t| Transaction.new(t, receipt) }.sort_by(&:purchased_at)
     end
 
     def where(props)
